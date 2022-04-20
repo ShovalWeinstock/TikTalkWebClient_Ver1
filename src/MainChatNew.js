2f25425f742e4dd76cac22c3d3d3f7905102d364
shoval <shoval.weinstock@live.biu.ac.il>
2022-04-20 09:51:34 +0300

    designed date of last msg + changed cursor to pointer when hovering over "click here"

--- src/MainChatNew.js
+++ src/MainChatNew.js
@@ -9,72 +9,6 @@ import TypingArea from './typingArea/TypingArea';
 import messages from "./messages/Chats";
 import MsgLoopCreator from './messages/MsgLoopCreator';
 
-
-// class MainChatNew extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = { username: props.username, contactList: contacts };
-//     }
-
-//     doSearch(q) {
-//         console.log(q);
-//         this.setState({
-//             contactList: contacts.filter((contacts) => contacts.name.includes(q))
-//         });
-//     }
-
-//     refreshList() {
-//         this.setState({
-//             contactList: [...contacts]
-//         });
-//     }
-
-//     render() {
-//         return (
-//             <div className="container">
-
-//                 <div className="leftSide">
-//                     <div className='header'>
-//                         <div className='profilePicture'>
-//                             <img src={defauldImg} className="cover"></img>
-//                         </div>
-//                         <h6>Contact name</h6>
-//                         <AddContact refreshList={(refreshList)} />
-//                     </div>
-
-//                     {/*Search Chat*/}
-//                     <Search doSearch={doSearch} />
-
-//                     {/*Chats list*/}
-//                     <div className="chatsList">
-//                         <ContactList contactlis={this.state.contactList} />
-//                     </div>
-//                 </div>
-
-//                 {/*Current chat*/}
-//                 <div className="rightSide">
-//                     <div className='header'>
-//                         <div className='profilePicture'>
-//                             <img src={defauldImg} className="cover"></img>
-//                         </div>
-//                         <h6>Contact name</h6>
-//                     </div>
-//                     {/*Conversation*/}
-//                     <div className='chat'>
-//                     </div>
-//                     {/*Input area*/}
-//                     <div className='chatInput'>
-//                         <TypingArea />
-//                     </div>
-
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
-
-
 function MainChatNew(props) {
 
     const [messageList, setMessageList] = useState(messages);
@@ -121,7 +55,6 @@ function MainChatNew(props) {
                 <div className="chatsList">
                     {/* the list of contacts gets the current state of contacts and messages */}
                     <ContactList contactlis={contactList} onContactClick={refreshCurrentChat} updatedMsg={messageList} />
-            <pre>{JSON.stringify(messageList,undefined,2)}</pre>
                 </div>
             </div>
 
--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import MainChatNew from './MainChatNew';
 import Login from './login/Login';
 import avocado from './avocado.jpg';
 
+//<MainChatNew user={{ username: "shoval", nickname: "shov", password: "12345678Aa", profilePic: avocado }} />
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <MainChatNew user={{ username: "shoval", nickname: "shov", password: "12345678Aa", profilePic: avocado }}/>
+    <MainChatNew user={{ username: "shoval", nickname: "shov", password: "12345678Aa", profilePic: avocado }} />
   </React.StrictMode>,
   document.getElementById('root')
 );
