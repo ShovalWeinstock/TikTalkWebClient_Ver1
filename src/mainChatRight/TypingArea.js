81b41dec95f928eb5c3c138ab95a304944baa0a8
itamar-st <81354824+itamar-st@users.noreply.github.com>
2022-04-23 14:46:38 +0300

    audio recorder working

--- src/mainChatRight/TypingArea.js
+++ src/mainChatRight/TypingArea.js
@@ -7,6 +7,7 @@ function TypingArea({ refreshChat, currChat, refreshContactList }) {
     const [currentMsg, setCurrentMsg] = useState('');
     const [uploadImgPopup, setUploadImgPopup] = useState(false);
     const [uploadVidPopup, setUploadVidPopup] = useState(false);
+    const [uploadAudPopup, setUploadAudPopup] = useState(false);
 
     // Add a new message
     const addMsg = (type, content) => {
@@ -28,6 +29,7 @@ function TypingArea({ refreshChat, currChat, refreshContactList }) {
 
             <UploadPopup trigger={uploadImgPopup} setTrigger={setUploadImgPopup} type="image" addMsg={addMsg} />
             <UploadPopup trigger={uploadVidPopup} setTrigger={setUploadVidPopup} type="video" addMsg={addMsg} />
+            <UploadPopup trigger={uploadAudPopup} setTrigger={setUploadAudPopup} type="audio" addMsg={addMsg} />
 
             {/*Message typing area*/}
             <input id="chatTypingArea" value={currentMsg} onChange={(e) => setCurrentMsg(e.target.value, currentMsg)}
@@ -64,7 +66,7 @@ function TypingArea({ refreshChat, currChat, refreshContactList }) {
                     </li>
                     {/*Send audio*/}
                     <li>
-                        <a class="dropdown-item" href="#">
+                        <a class="dropdown-item" href="#" onClick={() => { setUploadAudPopup(true) }}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mic-fill" viewBox="0 0 16 16">
                                 <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z" />
                                 <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z" />
